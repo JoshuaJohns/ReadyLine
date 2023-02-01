d3da76a36d938d6a3e76270bed4b1619cc1cf52e
Joshua Johns <joshuajohns28@outlook.com>
2023-02-01 15:07:17 -0600

    SQL scripts

---
+++ SQL/CreateDatabase.sql
@@ -0,0 +1,120 @@
+﻿USE [master]
+GO
+
+IF db_id('ReadyLine') IS NOT NULL
+BEGIN
+  ALTER DATABASE [ReadyLine] SET SINGLE_USER WITH ROLLBACK IMMEDIATE;
+  DROP DATABASE [ReadyLine]
+END
+GO
+
+CREATE DATABASE [ReadyLine]
+GO
+
+USE [ReadyLine]
+GO
+
+
+CREATE TABLE [User] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [FirebaseUserId] nvarchar(255) UNIQUE NOT NULL,
+  [Email] nvarchar(255) NOT NULL,
+  [FirstName] nvarchar(255) NOT NULL,
+  [LastName] nvarchar(255) NOT NULL,
+  [ImageUrl] nvarchar(255) NOT NULL,
+  [CreatedDate] datetime NOT NULL,
+  [HireDate] datetime,
+  [JobTitle] nvarchar(255) NOT NULL,
+  [UserTypeId] int NOT NULL
+)
+GO
+
+CREATE TABLE [UserType] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [Name] nvarchar(255) NOT NULL
+)
+GO
+
+CREATE TABLE [Vehicle] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [VehicleTypeId] int NOT NULL,
+  [MileageAtPMService] int NOT NULL,
+  [CurrentMileage] int,
+  [VehicleNumber] nvarchar(255) NOT NULL,
+  [IsApproved] bit,
+  [IsClaimed] bit,
+  [IsInShop] bit
+)
+GO
+
+CREATE TABLE [Report] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [UserId] int NOT NULL,
+  [VehicleId] int NOT NULL,
+  [Issue] nvarchar(255) NOT NULL,
+  [DateCreated] datetime NOT NULL,
+  [DateCompleted] datetime,
+  [CategoryId] int
+)
+GO
+
+CREATE TABLE [ReportTag] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [ReportId] int NOT NULL,
+  [TagId] int NOT NULL
+)
+GO
+
+CREATE TABLE [Tag] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [Status] nvarchar(255) NOT NULL
+)
+GO
+
+CREATE TABLE [VehicleType] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [Name] nvarchar(255) NOT NULL
+)
+GO
+
+CREATE TABLE [Category] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [Stage] nvarchar(255) NOT NULL
+)
+GO
+
+CREATE TABLE [ClaimVehicle] (
+  [Id] int PRIMARY KEY IDENTITY(1, 1),
+  [UserId] int NOT NULL,
+  [VehicleId] int NOT NULL,
+  [BeginDate] datetime,
+  [EndDate] datetime
+)
+GO
+
+ALTER TABLE [ClaimVehicle] ADD FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [ClaimVehicle] ADD FOREIGN KEY ([VehicleId]) REFERENCES [Vehicle] ([Id]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [User] ADD FOREIGN KEY ([UserTypeId]) REFERENCES [UserType] ([Id])
+GO
+
+ALTER TABLE [ReportTag] ADD FOREIGN KEY ([ReportId]) REFERENCES [Report] ([Id]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [Report] ADD FOREIGN KEY ([UserId]) REFERENCES [User] ([Id])
+GO
+
+ALTER TABLE [Report] ADD FOREIGN KEY ([VehicleId]) REFERENCES [Vehicle] ([Id]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [ReportTag] ADD FOREIGN KEY ([TagId]) REFERENCES [Tag] ([Id]) ON DELETE CASCADE
+GO
+
+ALTER TABLE [Vehicle] ADD FOREIGN KEY ([VehicleTypeId]) REFERENCES [VehicleType] ([Id])
+GO
+
+ALTER TABLE [Report] ADD FOREIGN KEY ([CategoryId]) REFERENCES [Category] ([Id])
+GO
---
+++ SQL/Seed.sql
@@ -0,0 +1,103 @@
+﻿SET IDENTITY_INSERT [User] ON
+INSERT INTO [User]
+    ([Id],[FirebaseUserId], [FirstName],[LastName], [Email], [CreatedDate],[HireDate],[JobTitle],[UserTypeId], [ImageUrl])
+VALUES
+    (1, '8XeC8bZveDYSMOYF4qxDglIVZyE2', 'Tyrus','Barkley','tyrus@gmail.com', SYSDATETIME(), NULL, 'Suppervisor', 2,'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShg8keaWuTWemET3-1mWqZae05N8W6SLGgGg&usqp=CAU' ),
+    (2, 'ynbkI8OOTrX2ryko9K00VE6eOS42','Thomas', 'Reed', 'thomas@gmail.com', SYSDATETIME(), NULL, 'Equipment Operator Senior', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShg8keaWuTWemET3-1mWqZae05N8W6SLGgGg&usqp=CAU'),
+    (3, 'vLA6fNDsV2f48xTRfuM92fHOJ362', 'Mike', 'Thompson','mike@gmail.com', SYSDATETIME(), NULL, 'Equipment Operator Senior', 1, 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShg8keaWuTWemET3-1mWqZae05N8W6SLGgGg&usqp=CAU');
+SET IDENTITY_INSERT [User] OFF
+
+
+SET IDENTITY_INSERT [Vehicle] ON
+INSERT INTO [Vehicle]
+    ([Id], [CurrentMileage], [MileageAtPMService], [VehicleNumber], [IsApproved], [IsClaimed], [IsInShop], [VehicleTypeId])
+VALUES
+    (1, 20000, 20000, '5386AD', 1, 1, 0, 1),
+    (2, 32000, 20000, '5387AD', 1, 0, 0, 1),
+    (3, 22000, 20000, '5388AD', 1, 1, 0, 1),
+    (4, 45230, 20000, '5342AD', 1, 1, 1, 1),
+    (5, 51000, 20000, '5142AD', 1, 0, 0, 2),
+    (6, 3000, 0, '5147AD', 1, 0, 0, 2),
+    (7, 2000, 0, '5887AD', 1, 0, 0, 3)
+SET IDENTITY_INSERT [Vehicle] OFF
+
+
+SET IDENTITY_INSERT [ClaimVehicle] ON
+INSERT INTO [ClaimVehicle]
+    ([Id], [BeginDate], [VehicleId], [UserId], [EndDate])
+VALUES
+    (1, SYSDATETIME(), 1, 1, NULL),
+    (2, SYSDATETIME(), 3, 2, NULL),
+    (3, SYSDATETIME(), 4, 3, NULL)
+SET IDENTITY_INSERT [ClaimVehicle] OFF
+
+
+SET IDENTITY_INSERT [UserType] ON
+INSERT INTO [UserType]
+    ([Id], [Name])
+VALUES
+    (1, 'Employee'),
+    (2, 'Supervizor'),
+    (3, 'Mechanic')
+SET IDENTITY_INSERT [UserType] OFF
+
+
+SET IDENTITY_INSERT [Report] ON
+INSERT INTO [Report]
+    ([Id], [UserId], [VehicleId], [Issue], [DateCreated], [DateCompleted], [CategoryId])
+VALUES
+    (1, 1, 1, 'PM Service', SYSDATETIME(), NULL, 1),
+    (2, 1, 6, 'PM Service', SYSDATETIME(), NULL, 1),
+    (3, 2, 2, 'PM Service', SYSDATETIME(), NULL, 1),
+    (4, 2, 3, 'Hydrolic Filter Leak', SYSDATETIME(), NULL, 2),
+    (5, 3, 5, 'PM Service', SYSDATETIME(), NULL, 1)
+SET IDENTITY_INSERT [Report] OFF
+
+SET IDENTITY_INSERT [ReportTag] ON
+INSERT INTO [ReportTag]
+    ([Id], [ReportId], [TagId])
+VALUES
+    (1, 1, 1),
+    (2, 1, 1),
+    (3, 1, 1),
+    (4, 1, 2),
+    (5, 1, 1)
+    
+SET IDENTITY_INSERT [ReportTag] OFF
+
+
+SET IDENTITY_INSERT [Tag] ON
+INSERT INTO [Tag]
+    ([Id], [Status])
+VALUES
+    (1, 'PM Serivice'),
+    (2, 'Hydrolic Leak'),
+    (3, 'Tire Change'),
+    (4, 'Fluid Top Off'),
+    (5, 'Grease'),
+    (6, 'Check Engine Light')
+    
+SET IDENTITY_INSERT [Tag] OFF
+
+
+
+SET IDENTITY_INSERT [VehicleType] ON
+INSERT INTO [VehicleType]
+    ([Id], [Name])
+VALUES
+    (1, 'Side Arm'),
+    (2, 'Rear Loader'),
+    (3, 'Front Loader')
+    
+SET IDENTITY_INSERT [VehicleType] OFF
+
+
+SET IDENTITY_INSERT [Category] ON
+INSERT INTO [Category]
+    ([Id], [Stage])
+VALUES
+    (1, 'Quick'),
+    (2, 'Moderate'),
+    (3, 'Dificult')
+    
+SET IDENTITY_INSERT [Category] OFF
